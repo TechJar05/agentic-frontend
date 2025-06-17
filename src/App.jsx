da4ea0166faac93eef55a8119f55f736cb4c5c1a
OmkarLatambale <omlatambale2002@gmail.com>
2025-06-17 22:39:25 +0530

    routing updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import LandingPage from './pages/LandingPage';
-// import LoginPage from './pages/LoginPage';
-// import RegisterPage from './pages/RegisterPage';
-// import MDdashboard from './pages/MDdashboard'
-// import ManageEmployee from './pages/ManageEmployee';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-//         <Route path="/dashboard" element={<MDdashboard />} />
-//         <Route path="/manage-employee" element={<ManageEmployee />} />
-
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 // src/App.js
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MDdashboard from './pages/MDdashboard';
